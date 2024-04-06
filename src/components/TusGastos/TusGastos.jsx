bf620e95c63b26ec724eac6172b62afeadcf95b0
Felipe Avila <juanfe.avilat@uniagustiniana.edu.co>
2024-04-05 19:37:57 -0500

    componenten tusgastos

--- src/components/TusGastos/TusGastos.jsx
+++ src/components/TusGastos/TusGastos.jsx
@@ -1,30 +1,40 @@
-import React, { useState } from "react";
+import React, { useState,useRef  } from "react";
 import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
-import CardGastos from "./components/CardGastos";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import CardGastosCC from "./components/CardGastosCC";
 import PerfectScrollbar from "perfect-scrollbar";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 import { Card } from "@mui/material";
+import CardGastosFijos from "./components/CardGastosFijos";
 
 function TusGastos({ userId }) {
   const [showForm, setShowForm] = useState(false);
   const [gastos, setGastos] = useState([]);
   const [nuevoGasto, setNuevoGasto] = useState({ categoria: "", monto: "" });
+  const [cards, setCards] = useState([]);
+  const carouselRef = useRef(null);
 
   const handleAgregarGasto = () => {
-    // Aquí normalmente enviarías la información al backend
-    // Pero para este ejemplo, simplemente vamos a añadir el gasto al estado local
-    setGastos([...gastos, nuevoGasto]);
-    setNuevoGasto({ categoria: "", monto: "" }); // Resetea el formulario
-    setShowForm(false); // Oculta el formulario
+    const cardStyle = cards.length === 0 ? "" : "ml-8"; 
+    const newCard = (
+      <div key={cards.length} className={cardStyle}>
+        <CardGastosCC userId={userId} />
+      </div>
+    );
+    setCards([...cards, newCard]);
+  };
+  const scroll = (scrollOffset) => {
+    carouselRef.current.scrollLeft += scrollOffset;
   };
 
   return (
-    <div className="mt-28 ml-4">
+    <div className="mt-[110px] ml-4">
       <div className="flex items-center">
         <span className="text-3xl font-bold">Tus Gastos</span>
         <IconButton
-          onClick={() => setShowForm(!showForm)}
+          onClick={handleAgregarGasto}
           color="primary"
           aria-label="add"
           style={{
@@ -37,10 +47,45 @@ function TusGastos({ userId }) {
         >
           <AddIcon />
         </IconButton>
+        <IconButton
+          onClick={() => scroll(-200)}
+          aria-label="previous"
+          style={{
+            borderRadius: "50%",
+            background: "white",
+            padding: "0.2rem",
+            left: "18px",
+            top: "3px",
+          }}
+        >
+          <NavigateBeforeIcon />
+        </IconButton>
+        <IconButton
+          onClick={() => scroll(200)}
+          aria-label="next"
+          style={{
+            borderRadius: "50%",
+            background: "white",
+            padding: "0.2rem",
+            left: "27px",
+            top: "3px",
+          }}
+        >
+          <NavigateNextIcon />
+        </IconButton>
       </div>
-       <CardGastos />
-      </div>
-    
+      <div 
+      ref={carouselRef} 
+      className="flex flex-nowrap overflow-x-hidden" 
+      style={{ scrollBehavior: 'smooth' }}
+    >
+       {cards.map((card, index) => (
+        <div key={index} className="flex-none"> 
+          {card}
+        </div>
+      ))}
+    </div>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+    <main className="dark">
+        <App />
+      </main>
     </NextUIProvider>
   </React.StrictMode>,
 )
