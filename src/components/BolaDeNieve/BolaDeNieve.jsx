33e323fe7b4cb3276ece54e1fb951d601b66a937
Felipe Avila <juanfe.avilat@uniagustiniana.edu.co>
2024-04-16 16:58:23 -0500

    Adding new modal

--- src/components/BolaDeNieve/BolaDeNieve.jsx
+++ src/components/BolaDeNieve/BolaDeNieve.jsx
@@ -1,65 +1,75 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardHeader } from "@nextui-org/react";
+import { Card, CardHeader, useDisclosure } from "@nextui-org/react";
+import ModalNieve from "./components/ModalNieve";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 function BolaDeNieve({ userId }) {
- // console.log("userId:", userId);
+  // console.log("userId:", userId);
 
   const [gastoMenor, setGastoMenor] = useState(null);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-    useEffect(() => {
-        const fetchGastosMenores = async () => {
-        // URL de los endpoints
-        const gastosFijosUrl = `http://localhost:8080/usuarios/${userId}/gastosfijos`;
-        const tarjetasCreditoUrl = `http://localhost:8080/usuarios/${userId}/tarjetascredito`;
+  useEffect(() => {
+    const fetchGastosMenores = async () => {
+      // URL de los endpoints
+      const gastosFijosUrl = `http://localhost:8080/usuarios/${userId}/gastosfijos`;
+      const tarjetasCreditoUrl = `http://localhost:8080/usuarios/${userId}/tarjetascredito`;
 
-        try {
-            // Obtiene los datos de ambos endpoints
-            const [gastosFijosResponse, tarjetasCreditoResponse] = await Promise.all([
-            fetch(gastosFijosUrl),
-            fetch(tarjetasCreditoUrl),
-            ]);
+      try {
+        // Obtiene los datos de ambos endpoints
+        const [gastosFijosResponse, tarjetasCreditoResponse] =
+          await Promise.all([fetch(gastosFijosUrl), fetch(tarjetasCreditoUrl)]);
 
-            const gastosFijosData = await gastosFijosResponse.json();
-            const tarjetasCreditoData = await tarjetasCreditoResponse.json();
+        const gastosFijosData = await gastosFijosResponse.json();
+        const tarjetasCreditoData = await tarjetasCreditoResponse.json();
 
-            // Combina los arrays de gastos en un solo array
-            const todosLosGastos = [
-            ...gastosFijosData.flatMap(gasto => gasto.gastos),
-            ...tarjetasCreditoData.flatMap(tarjeta => tarjeta.gastos),
-            ];
+        // Combina los arrays de gastos en un solo array
+        const todosLosGastos = [
+          ...gastosFijosData.flatMap((gasto) => gasto.gastos),
+          ...tarjetasCreditoData.flatMap((tarjeta) => tarjeta.gastos),
+        ];
 
-            // Encuentra el gasto con el valor más bajo
-            if (todosLosGastos.length > 0) {
-            const menor = todosLosGastos.reduce((min, gasto) => gasto.valorTotalGasto < min.valorTotalGasto ? gasto : min);
-            todosLosGastos.sort((a, b) => a.valorTotalGasto - b.valorTotalGasto);
+        // Encuentra el gasto con el valor más bajo
+        if (todosLosGastos.length > 0) {
+          const menor = todosLosGastos.reduce((min, gasto) =>
+            gasto.valorTotalGasto < min.valorTotalGasto ? gasto : min
+          );
+          todosLosGastos.sort((a, b) => a.valorTotalGasto - b.valorTotalGasto);
           //  console.log("Todos los gastos:", todosLosGastos);
-            setGastoMenor(menor);
+          setGastoMenor(menor);
           //  console.log("Gasto menor:", menor);
-            }
-        } catch (error) {
-            console.error("Error al obtener los gastos:", error);
         }
-        };
+      } catch (error) {
+        console.error("Error al obtener los gastos:", error);
+      }
+    };
 
-        if (userId) {
-        fetchGastosMenores();
-        }
-    }, [userId]);
-
+    if (userId) {
+      fetchGastosMenores();
+    }
+  }, [userId]);
   return (
     <Card className="shadow-none dark w-[1000px] h-[55px] ml-32 bg-gray-900">
-    <CardHeader className="flex justify-between items-center">
-      <div className="flex justify-start items-center">
-        <span className="text-lg font-bold whitespace-nowrap overflow-hidden overflow-ellipsis">
-          Método Bola de Nieve
-        </span>
-        {gastoMenor && (
-          <span className="ml-4">Gasto Menor: {gastoMenor.nombreGasto} - ${gastoMenor.valorTotalGasto}</span>
-        )}
-      </div>
-    </CardHeader>
-  </Card>
-  
+      <CardHeader className="flex justify-between items-center">
+        <div className="flex justify-start items-center">
+          <span className="text-lg font-bold whitespace-nowrap overflow-hidden overflow-ellipsis">
+            Método Bola de Nieve
+          </span>
+          <InfoOutlinedIcon
+            className="ml-1 mt-[3px] cursor-pointer"
+            onClick={onOpen}
+          />
+          {gastoMenor && (
+            <span className="ml-4">
+              Gasto Menor: {gastoMenor.nombreGasto} - $
+              {gastoMenor.valorTotalGasto}
+            </span>
+          )}
+        </div>
+      </CardHeader>
+      {/* Aquí pasamos las props al componente de modal para controlar su visibilidad */}
+      <ModalNieve isOpen={isOpen} onClose={() => onOpenChange(false)} />
+    </Card>
   );
 }
 
---
+++ src/components/BolaDeNieve/components/ModalNieve.jsx
@@ -0,0 +1,132 @@
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Divider,
+} from "@nextui-org/react";
+import snowballGif from "../../../assets/ModalSnowball.gif";
+import { useState } from "react";
+
+const ModalNieve = ({ isOpen, onClose }) => {
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false);
+
+  const handleToggleInfo = () => {
+    setShowAdditionalInfo(!showAdditionalInfo);
+  };
+
+  return (
+    <>
+      <Modal
+        backdrop="opaque"
+        isOpen={isOpen}
+        onClose={onClose}
+        radius="lg"
+        classNames={{
+          body: "py-6",
+          backdrop: "bg-[#0e294b]/50 backdrop-opacity-40",
+          base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#faefef] ",
+          header: "border-b-[1px] border-[#292f46]",
+          footer: "border-t-[1px] border-[#292f46]",
+          closeButton: "hover:bg-white/5 active:bg-white/10",
+        }}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-row justify-start items-center">
+                <span>El método bola de nieve</span>
+                <img
+                  src={snowballGif} // Asegúrate de que la ruta al archivo es correcta
+                  alt="Bola de nieve"
+                  className="w-10 h-50 mt-1" // Ajusta el tamaño como necesites
+                />
+              </ModalHeader>
+              <ModalBody>
+                {!showAdditionalInfo ? (
+                  <>
+                    <span className="text-lg font-bold">
+                      ¿Por qué se le llama “bola de nieve”?
+                    </span>
+                    <p className="text-sm">
+                      Como una bola de nieve que crece al rodar cuesta abajo,
+                      las deudas pueden aumentar al acumularse con más compras y
+                      el agregado constante de intereses. Con el método de bola
+                      de nieve, enfrentas este crecimiento pagando deudas de
+                      menor a mayor, liberándote poco a poco de la presión
+                      financiera. Este enfoque te motiva a continuar, creciendo
+                      tu capacidad de pago a medida que reduces tus deudas, una
+                      por una.
+                    </p>
+                    <Divider />
+                    <span className="text-lg font-bold">
+                      Implementando el método
+                    </span>
+                    <p className="text-sm">
+                      Este método se enfoca en liquidar deudas empezando por la
+                      más pequeña, independientemente de sus intereses, para
+                      luego seguir en orden ascendente. Aquí te mostramos cómo
+                      aplicarlo en 4 sencillos pasos:
+                      <br />
+                      <ul className="list-disc text-sm mt-1">
+                        <li>
+                          Presupuesta: Analiza tus ingresos y egresos para
+                          determinar cuánto puedes pagar de tus deudas cada mes.
+                        </li>
+                        <li>
+                          Ordena tus deudas: Enumera tus deudas desde la menor a
+                          la mayor cantidad adeudada.
+                        </li>
+                        <li>
+                          Comienza a pagar: Dedica lo más que puedas a saldar la
+                          deuda menor mientras mantienes los pagos mínimos en
+                          las demás.
+                        </li>
+                        <li>
+                          Continúa el ciclo: Tras saldar una deuda, redirige
+                          esos fondos a la siguiente, aumentando progresivamente
+                          tu capacidad de pago.
+                        </li>
+                      </ul>
+                    </p>
+                  </>
+                ) : (
+                  <div>
+                    {/* Cuerpo al pasar de contenido */}
+                    <span>
+                      Contenido adicional relacionado con el método bola de
+                      nieve.
+                    </span>
+                 
+                  </div>
+                )}
+              </ModalBody>
+
+              <ModalFooter>
+                <Button
+                  color="foreground"
+                  variant="light"
+                  onPress={handleToggleInfo}
+                >
+                  {showAdditionalInfo ? "Volver" : "Más información"}
+                </Button>
+                <Button
+                  color="foreground"
+                  variant="light"
+                  onPress={() => onClose(false)}
+                >
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalNieve;
